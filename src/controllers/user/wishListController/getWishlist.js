2af1b74b9369493bd165d98da79696a75894b716
ajeetkesherwani <kesherwaniajeet@gmail.com>
2025-08-11 11:26:03 +0530

    wishlist update

--- src/controllers/user/wishListController/getWishlist.js
+++ src/controllers/user/wishListController/getWishlist.js
@@ -1,17 +1,29 @@
 const Wishlist = require("../../../models/wishlist");
 const AppError = require("../../../utils/AppError");
 const catchAsync = require("../../../utils/catchAsync");
+const { successResponse } = require("../../../utils/responseHandler");
 
-exports.getWishlist = catchAsync(async(req, res, next) => {
+exports.getWishlist = catchAsync(async (req, res, next) => {
+  const user_Id = req.user.id;
+  if (!user_Id) {
+    return next(new AppError("id is required", 400));
+  }
 
-    const { user_Id } = req.params;
-     if(!user_Id){
-        return next(new AppError("id is required", 400));
-     }
+  const getList = await Wishlist.find({ user_Id }).populate(
+    "product_Id",
+    "name primary_image price discountedPrice"
+  );
+  if (!getList) return next(new AppError("Wishlist not found", 404));
 
-     const getList = await Wishlist.find({ user_Id }).populate("product_Id");
-     if(!getList) return next(new AppError("Wishlist not found", 404));
-
-     res.status(200).json({ status: true, message: "Wishlist found",count: getList.length, data: getList });
-
+  const data = {
+    count: getList.length,
+    list: getList,
+  };
+  //   successResponse(res, 200, "Wishlist found", data);
+  res.status(200).json({
+    status: true,
+    message: "Wishlist found",
+    count: getList.length,
+    data: getList,
+  });
 });
--- src/routes/user/wishlistRoutes.js
+++ src/routes/user/wishlistRoutes.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const { createWishlist } = require("../../controllers/user/wishListController/createWishlist");
-const { getWishlist } = require("../../controllers/user/wishListController/getWishlist");
-const { deleteWishlist } = require("../../controllers/user/wishListController/deleteWishlist");
+const {
+  userAuthenticate,
+} = require("../../controllers/user/authController/userAuthenticate");
+const {
+  createWishlist,
+} = require("../../controllers/user/wishListController/createWishlist");
+const {
+  getWishlist,
+} = require("../../controllers/user/wishListController/getWishlist");
+const {
+  deleteWishlist,
+} = require("../../controllers/user/wishListController/deleteWishlist");
 const router = express.Router();
 
-router.post("/create", createWishlist);
-router.get("/list/:user_Id", getWishlist);
-router.delete("/delete/:id", deleteWishlist);
+router.post("/create", userAuthenticate, createWishlist);
+router.get("/list", userAuthenticate, getWishlist);
+router.delete("/delete/:id", userAuthenticate, deleteWishlist);
 
 module.exports = router;
